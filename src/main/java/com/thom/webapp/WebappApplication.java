d1d73d38b51f1abdc1046e2a6d30836ea624c300
Thom988 <t.quillet@hotmail.fr>
2024-01-31 16:18:54 +0100

    Ajout d'un package configuration avec classe CustomProperties pour propriétés com.thom.webapp

--- src/main/java/com/thom/webapp/WebappApplication.java
+++ src/main/java/com/thom/webapp/WebappApplication.java
@@ -3,11 +3,14 @@ package com.thom.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class WebappApplication {
-
+    
+    
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
 	}
+	
 
 }
